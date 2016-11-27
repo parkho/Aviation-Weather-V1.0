76c517733b96f4a1d4655042ce4bc68930b9d04a
Parviz Khorrami <pkhorraminejad@ymail.com>
2016-11-27 20:26:04 +0330

    Version Check Is Needed

--- module/WTHR/WTHR.php
+++ module/WTHR/WTHR.php
@@ -3,7 +3,16 @@ class WTHR extends CodonModule
 	{
 		public function index() 
 			{
-                $this->show('weather/weather.php');
+				$revision = trim(file_get_contents(CORE_PATH.'/version'));
+			if($revision != 'simpilot 5.5.2')
+				{
+					echo '<center>phpVMS Version Installed Is Not Compatible With This Module!</center><br />';
+					echo '<center>phpVMS Version Installed: '.$revision.'</center>';
+				}
+			else
+				{
+					$this->show('weather/weather.php');
+				}
 			}
 	
 		public function metar()
